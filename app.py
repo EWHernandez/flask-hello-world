988e8f460dc26ded1468d19932722ce300cd8fbe
EWHernandez <erhe3221@colorado.edu>
2023-11-04 21:38:57 +0000

    comments added to routes.

--- app.py
+++ app.py
@@ -3,16 +3,19 @@ import psycopg2
 
 app = Flask(__name__)
 
+#initial route, will be first page dipayed with no specified routing
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+#testing extension to connecting to render_db
 @app.route('/db_test')
 def testing():
     conn = psycopg2.connect("postgres://lab10_db_d7cl_user:CA2aFN2fD0vghFJFPBSCOx4A9kWZdR46@dpg-cl3anqauuipc738ae48g-a/lab10_db_d7cl")
     conn.close()
     return "Database Connection Successful"
 
+#creates the basketball table
 @app.route('/db_create')
 def creating():
     conn = psycopg2.connect("postgres://lab10_db_d7cl_user:CA2aFN2fD0vghFJFPBSCOx4A9kWZdR46@dpg-cl3anqauuipc738ae48g-a/lab10_db_d7cl")
@@ -30,6 +33,7 @@ def creating():
     conn.close()
     return "Basketball Table Successfully Created"
 
+#Inserts provided info into basketball table
 @app.route('/db_insert')
 def inserting():
     conn = psycopg2.connect("postgres://lab10_db_d7cl_user:CA2aFN2fD0vghFJFPBSCOx4A9kWZdR46@dpg-cl3anqauuipc738ae48g-a/lab10_db_d7cl")
@@ -46,6 +50,7 @@ def inserting():
     conn.close()
     return "Basketball Table Successfully Populated"
 
+#queries data from table, returns formatted data
 @app.route('/db_select')
 def selecting():
     conn = psycopg2.connect("postgres://lab10_db_d7cl_user:CA2aFN2fD0vghFJFPBSCOx4A9kWZdR46@dpg-cl3anqauuipc738ae48g-a/lab10_db_d7cl")
@@ -65,6 +70,7 @@ def selecting():
     response_string+="</table>"
     return response_string
 
+#clears the table from the render db
 @app.route('/db_drop')
 def dropping():
     conn = psycopg2.connect("postgres://lab10_db_d7cl_user:CA2aFN2fD0vghFJFPBSCOx4A9kWZdR46@dpg-cl3anqauuipc738ae48g-a/lab10_db_d7cl")
